6d25cffcd0ce0ba73c796436298b0d84e80b4a01
dave <davide.dcube@gmail.com>
2022-06-24 00:15:37 +0200

    Building interactive CLI

---
+++ app/src/main/kotlin/red/tetracube/ApplicationGlobal.kt
@@ -0,0 +1,27 @@
+package red.tetracube
+
+object ApplicationGlobal {
+
+    // Reset
+    const val RESET = "\u001b[0m" // Text Reset
+
+    // Regular Colors
+    const val BLACK = "\u001b[0;30m" // BLACK
+    const val RED = "\u001b[0;31m" // RED
+    const val GREEN = "\u001b[0;32m" // GREEN
+    const val YELLOW = "\u001b[0;33m" // YELLOW
+    const val BLUE = "\u001b[0;34m" // BLUE
+    const val PURPLE = "\u001b[0;35m" // PURPLE
+    const val CYAN = "\u001b[0;36m" // CYAN
+    const val WHITE = "\u001b[0;37m" // WHITE
+
+    // Bold
+    const val BLACK_BOLD = "\u001b[1;30m" // BLACK
+    const val RED_BOLD = "\u001b[1;31m" // RED
+    const val GREEN_BOLD = "\u001b[1;32m" // GREEN
+    const val YELLOW_BOLD = "\u001b[1;33m" // YELLOW
+    const val BLUE_BOLD = "\u001b[1;34m" // BLUE
+    const val PURPLE_BOLD = "\u001b[1;35m" // PURPLE
+    const val CYAN_BOLD = "\u001b[1;36m" // CYAN
+    const val WHITE_BOLD = "\u001b[1;37m" // WHITE
+}
